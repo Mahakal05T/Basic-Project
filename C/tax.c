64f81293ef0949ec316d9554331f064384bea6dc
ayush <vinodsingh47928@gmail.com>
2023-09-18 17:46:20 +0530

    some changes done in this file

--- C/tax.c
+++ C/tax.c
@@ -8,15 +8,15 @@ int main(){
     {
         printf("\nYou don't have to pay tax in your salary: %d",sal);
     }
-    else if(sal > 250000 && sal < 500000)
+    else if(sal >= 250000 && sal <= 500000)
     {
         printf("\nYou have to pay 5 percent tax in your salary: %d",sal);
     }
-    else if (sal > 500000 && sal < 1000000)
+    else if (sal >= 500000 && sal <= 1000000)
     {
         printf("\nYou have to pay 20 percent tax in your salary: %d",sal);
     }
-    else if(sal > 1000000){
+    else if(sal >= 1000000){
         printf ("\nyou have to pay 30 percent tax in your salary: %d",sal);
     }
 return 0 ;
