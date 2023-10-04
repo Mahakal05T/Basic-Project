e367409a98cab9a269a596d66a3027e4764099aa
ayush <vinodsingh47928@gmail.com>
2023-10-04 11:33:44 +0530

    time pass

---
+++ Python/pass-fail.py
@@ -0,0 +1,12 @@
+grade = int(input("enter  your grade: "))
+
+if grade >= 90:
+    print("A")
+elif 80 <= grade < 90:
+    print("B")
+elif 70 <= grade < 80:
+    print("C")
+elif 60 <= grade < 70:
+    print("D")
+else:
+    print("F")
---
+++ Python/sum-array.py
@@ -0,0 +1,8 @@
+myarr = []
+a=int(input("enter list limit: "))
+for x in range (0,a):
+    b=int(input("enter a elements: "))
+    myarr.append(b)
+print(myarr)
+print("sum of array",sum(myarr))
+print("largest element in array",max(myarr))
